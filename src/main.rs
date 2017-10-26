88afbfc99fb42051001ba15bf284360f30821bec
hummingly <31522351+hummingly@users.noreply.github.com>
2017-10-26 10:14:59 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -269,7 +269,7 @@ fn release_resources(app: &mut MultiFormatsText) {
     unsafe {
         safe_release(app);
 
-        if !app.dwrite_factory.is_null() {
+        if !app.d2d1_factory.is_null() {
             (*app.d2d1_factory).Release();
             app.d2d1_factory = null_mut();
         }
